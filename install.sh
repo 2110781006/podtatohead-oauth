110a1eec494e8e901a86982886b7456623264c34
Michael Schmit <m.schmit@pke.at>
2021-09-21 22:02:04 +0200

    sdf

--- install.sh
+++ install.sh
@@ -6,3 +6,21 @@ sudo yum install docker -y
 sudo service docker start
 sudo usermod -a -G docker ec2-user
 echo "install docker finished"
+sudo docker run -d -p 8080:9000 ghcr.io/podtato-head/podtatoserver:v0.1.2
+sudo export PUBLIC_IPV4_ADDRESS="$(curl http://169.254.169.254/latest/meta-data/public-ipv4)"
+sudo cat << EOF
+
+
+=======
+Application name: 
+-- podtatohead-on-aws
+
+Homepage URL:     
+- https://$PUBLIC_IPV4_ADDRESS.nip.io
+
+Authorization callback URL: 
+- https://$PUBLIC_IPV4_ADDRESS.nip.io/oauth2/callback
+=======
+
+
+EOF
