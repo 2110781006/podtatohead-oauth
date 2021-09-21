f75c7dad41454efb7e6aa29d81ef33c9cef8fcd6
Michael Schmit <m.schmit@pke.at>
2021-09-21 22:23:19 +0200

    sdf

--- install.sh
+++ install.sh
@@ -48,10 +48,14 @@ sudo mv oauth2-proxy-v7.1.3.linux-amd64/oauth2-proxy /opt/oauth2-proxy/
 
 export COOKIE_SECRET=$(python -c 'import os,base64; print(base64.urlsafe_b64encode(os.urandom(16)).decode())')
 
-export GITHUB_USER==$( echo "Y0t+mPZbCH87A5FTjHpAH/rvaB/3s+FQS7IFmEtdszvKaEdMwjVjmzjCGQPdBSi7 fXf3uBMertBJ6lBE2RUCm39BGRVuANUIzuf6lYYgGsdyW1mdLiHn1UCOr3gAnYWI zn+CZOV3tUceTh2hnG3am9b8B0SQ9VqWqOlfkCHNpfw=" | openssl enc -base64 -d | openssl rsautl -decrypt -inkey ~/podtatohead-oauth/myPrivate.pem)
+export GITHUB_USER=$( echo "Y0t+mPZbCH87A5FTjHpAH/rvaB/3s+FQS7IFmEtdszvKaEdMwjVjmzjCGQPdBSi7 fXf3uBMertBJ6lBE2RUCm39BGRVuANUIzuf6lYYgGsdyW1mdLiHn1UCOr3gAnYWI zn+CZOV3tUceTh2hnG3am9b8B0SQ9VqWqOlfkCHNpfw=" | openssl enc -base64 -d | openssl rsautl -decrypt -inkey ~/podtatohead-oauth/myPrivate.pem)
+export GITHUB_CLIENT_ID=$( echo "HNUWSVtP3GnQX0K4py6+Pu/qR0ogvAuGTAmWY5Io4ypIs3j3EDB8mAgk3NOyKvV1 VYaAEmIgpVt86S5ilU6TwGT0z8Ac2LePaWzOL3/wGOzngM2mfKoVZsAJVYDhx8OK wEIxvBgK2qq1dfTjmOK4P7G8nJvRgec2dC7NtU25tlc=" | openssl enc -base64 -d | openssl rsautl -decrypt -inkey ~/podtatohead-oauth/myPrivate.pem)
+export GITHUB_CLIENT_SECRET=$( echo "U2qnao3bYt1XpH+L5gJ40ZYGeXSjkqIEW+2VRAf9SfhbZARwnKWLhsgHDGyQ77jh BELk4i2MKyevccuBh8E/nwTxXg7HfXJoYqVn+1pKJwv943XINCleHX5HE30eXOV6 8daElOgUOShrjpKiGfeNqSEeOUZWsYgYmStZRQoeIZE=" | openssl enc -base64 -d | openssl rsautl -decrypt -inkey ~/podtatohead-oauth/myPrivate.pem)
+export PUBLIC_URL=$(curl http://169.254.169.254/latest/meta-data/public-ipv4).nip.io
+echo "-------------------------------"
 echo $GITHUB_USER
-#export GITHUB_CLIENT_ID=<GITHUB_CLIENT_ID>
-#export GITHUB_CLIENT_SECRET=<GITHUB_CLIENT_SECRET>
-#export PUBLIC_URL=$(curl http://169.254.169.254/latest/meta-data/public-ipv4).nip.io
+echo $GITHUB_CLIENT_ID
 
-#sudo /opt/oauth2-proxy/oauth2-proxy --github-user="${GITHUB_USER}"  --cookie-secret="${COOKIE_SECRET}" --client-id="${GITHUB_CLIENT_ID}" --client-secret="${GITHUB_CLIENT_SECRET}" --email-domain="*" --upstream=http://127.0.0.1:8080 --provider github --cookie-secure false --redirect-url=https://${PUBLIC_URL}/oauth2/callback --https-address=":443" --force-https --tls-cert-file=/etc/letsencrypt/live/$PUBLIC_URL/fullchain.pem --tls-key-file=/etc/letsencrypt/live/$PUBLIC_URL/privkey.pem
+echo $GITHUB_CLIENT_SECRET
+
+sudo /opt/oauth2-proxy/oauth2-proxy --github-user="${GITHUB_USER}"  --cookie-secret="${COOKIE_SECRET}" --client-id="${GITHUB_CLIENT_ID}" --client-secret="${GITHUB_CLIENT_SECRET}" --email-domain="*" --upstream=http://127.0.0.1:8080 --provider github --cookie-secure false --redirect-url=https://${PUBLIC_URL}/oauth2/callback --https-address=":443" --force-https --tls-cert-file=/etc/letsencrypt/live/$PUBLIC_URL/fullchain.pem --tls-key-file=/etc/letsencrypt/live/$PUBLIC_URL/privkey.pem
