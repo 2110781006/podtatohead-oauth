ff7bf0bfe5c70f4245f8499f6c899433c5729188
Michael Schmit <m.schmit@pke.at>
2021-09-21 21:12:16 +0200

    rename

---
+++ mgm.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "start git install"
+yum install git
+echo "git install finished"
+echo "git clone"
+git clone https://github.com/2110781006/podtatohead-oauth.git
+echo "git clone finished"
