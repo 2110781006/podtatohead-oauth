0963fa05f457a53d3bad61f4f145e1a84c71f140
Michael Schmit <m.schmit@pke.at>
2021-09-21 21:53:27 +0200

    sdf

--- mgm.sh
+++ mgm.sh
@@ -4,6 +4,7 @@ echo "start git install"
 sudo yum install git -y
 echo "git install finished"
 echo "git clone"
+sudo rm podtatohead-oauth -R -f
 sudo git clone https://github.com/2110781006/podtatohead-oauth.git
 echo "git clone finished"
 cd /podtatohead-oauth/
